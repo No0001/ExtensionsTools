d4e919b4f3c77cc954e25004d58e0fe80f26048f
大鈞 簡 <jyun03@ptc-nec.com.tw>
2019-01-30 11:26:14 +0800

    init

---
+++ ExtensionsTools/ExtensionsTools/BytesExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class BytesExtension
+{
+    /// <summary>
+    /// btye轉單位
+    /// </summary>
+    /// <param name="bytes"></param>
+    /// <returns></returns>
+    public static string FormatBytes(long bytes)
+    {
+        string[] Suffix = { "B", "KB", "MB", "GB", "TB" };
+        int i;
+        double dblSByte = bytes;
+        for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+        {
+            dblSByte = bytes / 1024.0;
+        }
+        return $"{dblSByte.ToString("0")}{Suffix[i]}";
+    }
+
+}
---
+++ ExtensionsTools/ExtensionsTools/PropertyInfoExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class PropertyInfoExtensions
+{
+    /// <summary>
+    /// 取得ClassAttributValue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TAttribute"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="prop"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static TValue GetClassAttributValue<T, TAttribute, TValue>(this T prop, Func<TAttribute, TValue> value) where TAttribute : Attribute where T : class
+    {
+        if (prop.GetType().GetCustomAttributes(typeof(TAttribute), true).FirstOrDefault() is TAttribute att)
+        {
+            return value(att);
+        }
+        return default(TValue);
+    }
+
+    /// <summary>
+    /// 取得欄位AttributValue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TAttribute"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="propertyName"></param>
+    /// <param name="className"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static TValue GetPropertyAttributValue<T, TAttribute, TValue>(this string propertyName, T className, Func<TAttribute, TValue> value) where TAttribute : Attribute where T : class
+    {
+        var prop = className.GetType().GetProperty(propertyName);
+
+        if (prop.GetCustomAttributes(typeof(TAttribute), true).FirstOrDefault() is TAttribute att)
+        {
+            return value(att);
+        }
+        return default(TValue);
+    }
+}
